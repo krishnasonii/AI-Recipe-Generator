dd7b7f55bcbf1744907b5daa91a7fe09d35abfc5
Krishna Soni <krishnasoni.8596@gmail.com>
2026-01-17 19:33:01 +0530

    api changed

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export const generateRecipe = (ingredients) =>
-  API.post("/recipe/generate", { ingredients });
+  API.post("/api/recipe/generate", { ingredients });
